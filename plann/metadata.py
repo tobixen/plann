74676274b755340aca74f29a651a7452186f224e
Tobias Brox <tobias@redpill-linpro.com>
2025-11-12 15:18:39 +0100

    Licence fix
    Replace reference to GPL3+ with GPL-3.0, possible solution for https://github.com/tobixen/plann/issues/23 ?

--- plann/metadata.py
+++ plann/metadata.py
@@ -1,9 +1,9 @@
 metadata = {
-    "version": "1.1.dev",
+    "version": "1.1.dev1",
     "author": "Tobias Brox",
     "author_short": "tobixen",
     "copyright": "Copyright 2013-2024, Tobias Brox and contributors",
-    "license": "GPLv3+",
+    "license": "GPL-3.0",
     "license_url": "http://www.gnu.org/licenses/gpl-3.0-standalone.html",
     "maintainer": "Tobias Brox",
     "author_email": "t-plann@tobixen.no",
